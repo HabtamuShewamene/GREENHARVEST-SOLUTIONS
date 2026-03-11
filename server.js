83f33ebf875eb565cc5d9e301195d26bff0d60a7
Habtamu Shewamene <habtamushewamene905@gmail.com>
2026-03-11 01:16:53 -0700

    create the folder structure of the backend

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 
 const { pool, connectDB } = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.get("/", async (req, res) => {
   res.status(200).send("Agricultural Ecommerce API Running");
